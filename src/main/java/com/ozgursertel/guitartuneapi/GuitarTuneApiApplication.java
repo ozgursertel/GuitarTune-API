6d4791c3912eceb91b4c6ca4b2485d752d84ec71
ozgursertel <sertel.ozgur@gmail.com>
2023-10-18 11:08:42 +0300

    Add existing project file to git

---
+++ src/main/java/com/ozgursertel/guitartuneapi/GuitarTuneApiApplication.java
@@ -0,0 +1,13 @@
+package com.ozgursertel.guitartuneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarTuneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarTuneApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ozgursertel/guitartuneapi/GuitarTuneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozgursertel.guitartuneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitarTuneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
