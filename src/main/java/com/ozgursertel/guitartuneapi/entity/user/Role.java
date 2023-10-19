96984ba4305338da37b081d714b049e96e6b6433
ozgursertel <sertel.ozgur@gmail.com>
2023-10-20 00:38:01 +0300

    Spring Security Implementation

---
+++ src/main/java/com/ozgursertel/guitartuneapi/entity/user/Role.java
@@ -0,0 +1,6 @@
+package com.ozgursertel.guitartuneapi.entity.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/ozgursertel/guitartuneapi/repository/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ozgursertel.guitartuneapi.repository.user;
+
+import com.ozgursertel.guitartuneapi.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByUsername(String username);
+}
